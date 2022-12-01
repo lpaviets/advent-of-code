8fde6be526d43763b5a8bde5cbecb173c19ac4b6
lpaviets <leo.paviet.salomon@gmail.com>
2022-12-01 08:01:00 +0100

    Day 1

--- aoc2022/src/ex1.lisp
+++ aoc2022/src/ex1.lisp
@@ -1,6 +1,23 @@
 (in-package #:aoc2022/ex1)
 
-(defun answer-ex-1-1 ())
+(defun read-elves (file)
+  (org.numbra.perso:read-file-as-lines-blocks file
+                                              :parse 'parse-integer))
 
-(defun answer-ex-1-2 ())
+(defun count-calories (elf)
+  (reduce #'+ elf))
 
+(defun count-max-calories (elves)
+  (reduce #'max elves :key #'count-calories))
+
+(defun count-k-max-calories (elves k)
+  (let ((sorted-elves (sort elves #'> :key #'count-calories)))
+    (reduce #'+ sorted-elves :end k :key #'count-calories)))
+
+(defun answer-ex-1-1 ()
+  (let ((elves (read-elves "../inputs/input1")))
+    (count-max-calories elves)))
+
+(defun answer-ex-1-2 ()
+  (let ((elves (read-elves "../inputs/input1")))
+    (count-k-max-calories elves 3)))
