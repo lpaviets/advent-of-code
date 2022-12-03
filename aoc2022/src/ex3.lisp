294ba8aec646dc560f58b167a2513e06b8d29203
lpaviets <leo.paviet.salomon@gmail.com>
2022-12-03 09:29:13 +0100

    Day 3: not that great of a solution but it works

--- aoc2022/src/ex3.lisp
+++ aoc2022/src/ex3.lisp
@@ -1,6 +1,70 @@
 (in-package #:aoc2022/ex3)
 
-(defun answer-ex-3-1 ())
+;;;; NOTE: not the best code, I do everywhere as if the intersection of
+;;;; two compartments (part 1) or of the three bags in a group (part 2)
+;;;; could be more than a single item
+;;;; It is not a problem efficiency-wise, but it means more code and
+;;;; less clarity
 
-(defun answer-ex-3-2 ())
+(defun compartments-from-bag (bag)
+  (let ((size (/ (length bag) 2)))
+    (list (ht-from-sequence bag :end size)
+          (ht-from-sequence bag :start size))))
 
+(defun add-item-to-compartment (item cp)
+  (setf (gethash item cp) t))
+
+(defun item-in-compartment-p (item cp)
+  (gethash item cp))
+
+(defun compartments-intersection (cp1 cp2)
+  (let ((intersection (make-hash-table)))
+    (do-hashkeys (x cp1)
+      (if (item-in-compartment-p x cp2)
+          (add-item-to-compartment x intersection)))
+    (do-hashkeys (x cp2)
+      (if (item-in-compartment-p x cp1)
+          (add-item-to-compartment x intersection)))
+    intersection))
+
+(defun item-priority (item)
+  (cond
+    ((lower-case-p item)
+     (1+ (- (char-code item) (char-code #\a))))
+    ((upper-case-p item)
+     (+ 27 (- (char-code item) (char-code #\A))))
+    (t (error "~S is not an item, i.e. a alphabetic character~%" item))))
+
+(defun bag-priority (bag)
+  (destructuring-bind (cp1 cp2)
+      (compartments-from-bag bag)
+    (let ((total 0))
+      (do-hashkeys (x (compartments-intersection cp1 cp2))
+        (incf total (item-priority x)))
+      total)))
+
+;;; Part 2
+(defun bags-split-in-group (bags)
+  (loop :for (a b c) :on bags :by #'cdddr
+        :collect (list
+                  (ht-from-sequence a)
+                  (ht-from-sequence b)
+                  (ht-from-sequence c))))
+
+(defun group-intersection (group)
+  (reduce #'compartments-intersection group))
+
+(defun group-priority (group)
+  (let ((total 0))
+    (do-hashkeys (x (group-intersection group))
+      (incf total (item-priority x)))
+    total))
+
+(defun answer-ex-3-1 ()
+  (let ((bags (read-file-as-lines "../inputs/input3")))
+    (reduce #'+ bags :key #'bag-priority)))
+
+(defun answer-ex-3-2 ()
+  (let* ((bags (read-file-as-lines "../inputs/input3"))
+         (groups (bags-split-in-group bags)))
+    (reduce #'+ groups :key #'group-priority)))
