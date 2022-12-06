556474f46442b497d216c9b1f7ae459c3ccea449
lpaviets <leo.paviet.salomon@gmail.com>
2022-12-06 07:36:03 +0100

    Day 6: terribly inefficient but input is small, so it's OK

--- aoc2022/src/ex6.lisp
+++ aoc2022/src/ex6.lisp
@@ -1,6 +1,14 @@
 (in-package #:aoc2022/ex6)
 
-(defun answer-ex-6-1 ())
+;;; "Slow" solution (but still about ONE ms on a virtual machine ...)
+(defun pos-signal (sig window)
+  (loop :for start :below (- (length sig) window)
+        :for end = (+ start window)
+        :when (apply #'char/= (coerce (subseq sig i end) 'list))
+          :do (return end)))
 
-(defun answer-ex-6-2 ())
+(defun answer-ex-6-1 ()
+  (pos-signal (read-file-one-line "../inputs/input6") 4))
 
+(defun answer-ex-6-2 ()
+  (pos-signal (read-file-one-line "../inputs/input6") 14))
