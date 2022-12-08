72e43ec7f7202a322524d6ce52d120595b3a5630
lpaviets <leo.paviet.salomon@gmail.com>
2022-12-08 07:35:28 +0100

    Day 8, there might be some code duplication but not that easy to fix

--- aoc2022/src/ex8.lisp
+++ aoc2022/src/ex8.lisp
@@ -1,6 +1,60 @@
 (in-package #:aoc2022/ex8)
 
-(defun answer-ex-8-1 ())
+(defun forest-height (forest)
+  (array-dimension forest 0))
 
-(defun answer-ex-8-2 ())
+(defun forest-width (forest)
+  (array-dimension forest 1))
 
+(defun tree-at (i j forest)
+  (aref forest i j))
+
+(defun visible-tree-p (i j forest)
+  (let ((height (tree-at i j forest)))
+    (or (loop :for k :from 0 :below i
+              :always (< (tree-at k j forest) height))
+        (loop :for k :from (1+ i) :below (forest-height forest)
+              :always (< (tree-at k j forest) height))
+        (loop :for k :from 0 :below j
+              :always (< (tree-at i k forest) height))
+        (loop :for k :from (1+ j) :below (forest-width forest)
+              :always (< (tree-at i k forest) height)))))
+
+(defun inside-visible-trees-count (forest)
+  (loop :for i :from 1 :below (1- (forest-height forest))
+        :sum (loop :for j :from 1 :below (1- (forest-width forest))
+                   :count (visible-tree-p i j forest))))
+
+(defun outside-visible-trees-count (forest)
+  (+ (* 2 (forest-height forest))
+     (* 2 (forest-width forest))
+     -4))
+
+(defun scenic-score (i j forest)
+  (let ((height (tree-at i j forest)))
+    (* (loop :for k :downfrom (1- i) :to 0
+             :count 1
+             :while (< (tree-at k j forest) height))
+       (loop :for k :from (1+ i) :below (forest-height forest)
+             :count 1
+             :while (< (tree-at k j forest) height))
+       (loop :for k :downfrom (1- j) :to 0
+             :count 1
+             :while (< (tree-at i k forest) height))
+       (loop :for k :from (1+ j) :below (forest-width forest)
+             :count 1
+             :while (< (tree-at i k forest) height)))))
+
+(defun highest-scenic-score (forest)
+  (let ((max-score 0))
+   (do-array (i j x forest max-score)
+     (setf max-score (max max-score (scenic-score i j forest))))))
+
+(defun answer-ex-8-1 ()
+  (let ((forest (read-file-as-array "../inputs/input8")))
+    (+ (inside-visible-trees-count forest)
+       (outside-visible-trees-count forest))))
+
+(defun answer-ex-8-2 ()
+  (let ((forest (read-file-as-array "../inputs/input8")))
+    (highest-scenic-score forest)))
