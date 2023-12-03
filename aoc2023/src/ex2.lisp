8221692e2d5670555d79432596f5f6b9a3c9cb0d
Leo Paviet Salomon <leo.paviet.salomon@gmail.com>
2023-12-03 12:48:42 +0100

    Day 2 and 3: day 3 is of quite poor quality

--- aoc2023/src/ex2.lisp
+++ aoc2023/src/ex2.lisp
@@ -1,6 +1,48 @@
 (in-package #:aoc2023/ex2)
 
-(defun answer-ex-2-1 ())
+;; Part 1
+(defun split-game-in-rounds (game)
+  (cdr (ppcre:split "[:;] " game))) ; CDR to remove "Game n: " part
 
-(defun answer-ex-2-2 ())
+(defun parse-round (round)
+  (loop :for cube :in (ppcre:split ", " round)
+        :for (num col) = (ppcre:split " " cube)
+        :collect (cons (read-from-string col) (parse-integer num))))
 
+(defun parse-game (line)
+  (mapcar 'parse-round (split-game-in-rounds line)))
+
+(defun amount-colour-drawn (colour round)
+  (or (cdr (assoc colour round))
+      0))
+
+(defun valid-round-p (round)
+  (and (<= (amount-colour-drawn 'red round) 12)
+       (<= (amount-colour-drawn 'green round) 13)
+       (<= (amount-colour-drawn 'blue round) 14)))
+
+(defun valid-game-p (game)
+  (every 'valid-round-p game))
+
+;; Part 2
+(defun merge-rounds (round1 round2)
+  (loop :for colour :in '(red green blue)
+        :for col1 = (amount-colour-drawn colour round1)
+        :for col2 = (amount-colour-drawn colour round2)
+        :collect (cons colour (max col1 col2))))
+
+(defun minimal-bag-for-game (game)
+  (reduce 'merge-rounds game))
+
+(defun power-game (game)
+  (reduce '* (minimal-bag-for-game game) :key 'cdr))
+
+(defun answer-ex-2-1 ()
+  (loop :for i :from 1
+        :for game :in (read-file-as-lines "../inputs/input2.txt" :parse 'parse-game)
+        :when (valid-game-p game)
+          :sum i))
+
+(defun answer-ex-2-2 ()
+  (reduce '+ (read-file-as-lines "../inputs/input2.txt" :parse 'parse-game)
+          :key 'power-game))
--- aoc2023/src/ex3.lisp
+++ aoc2023/src/ex3.lisp
@@ -1,6 +1,97 @@
 (in-package #:aoc2023/ex3)
 
-(defun answer-ex-3-1 ())
+(defclass anchored-number ()
+  ((val :initarg :val :accessor val)
+   (line :initarg :line :accessor line)
+   (col-start :initarg :start :accessor col-start)
+   (col-end :initarg :end :accessor col-end)))
 
-(defun answer-ex-3-2 ())
+;; Part 1
 
+;; Many other solutions. One could read the whole file as a single
+;; line (containing newlines), and find the position of numbers in
+;; that line, then convert them back to array positions.
+(defun get-number-at-pos (grid i j)
+  "Return the next number in GRID, starting from position (I, J)"
+  (let* ((w (array-dimension grid 1))
+         (h (array-dimension grid 0)))
+    (cond
+      ((>= j (1- w))
+       (if (>= i (1- h))
+           nil
+           (get-number-at-pos grid (1+ i) 0)))
+      ((digit-char-p (aref grid i j))
+       (loop :with res = (digit-char-p (aref grid i j))
+             :for new-j :from (1+ j) :below w
+             :for new-digit = (digit-char-p (aref grid i new-j))
+             :while new-digit
+             :do (setf res (+ new-digit (* 10 res)))
+             :finally (return (values (make-instance 'anchored-number :val res
+                                                                      :line i
+                                                                      :start j
+                                                                      :end (1- new-j))
+                                      (list i new-j)))))
+      (t (loop :for new-j :from j :below w
+               :until (digit-char-p (aref grid i new-j))
+               :finally (return (get-number-at-pos grid i new-j)))))))
+
+(defun get-all-numbers (grid)
+  (loop :for i = 0 :then (first pos)
+        :for j = 0 :then (second pos)
+        :for (num pos) = (multiple-value-list (get-number-at-pos grid i j))
+        :while num
+        :collect num))
+
+(defun grid-symbol-p (grid i j)
+  (let ((x (aref grid i j)))
+    (and (not (digit-char-p x))
+         (not (char= x #\.)))))
+
+(defun number-valid-p (grid num)
+  (loop :for j :from (col-start num) :to (col-end num)
+        :for neighbours = (neighbours (line num) j grid :diagonal t)
+        :thereis (loop :for (ni nj) :in neighbours
+                       :thereis (grid-symbol-p grid ni nj))))
+
+(defvar *gears* (make-hash-table :test 'equal))
+(defun collect-maybe-gears (grid)
+  (do-array (i j x grid)
+    (when (char= x #\*)
+      (setf (gethash (list i j) *gears*) (cons 0 1)))))
+
+(defun number-adjacent-p (i j num)
+  (let ((ni (line num))
+        (start (col-start num))
+        (end (col-end num)))
+    (or (and (= ni i) (or (= start (1+ j))  ; same line, to the left
+                          (= end (1- j))))  ; same line, to the right
+        (and (or (= ni (1- i))              ; line below
+                 (= ni (1+ i)))             ; line above
+             (<= (1- start) j (1+ end)))))) ; in the bounds
+
+(defun gear-p (i j nums)
+  (loop :with adjacent = 0
+        :with res = 1
+        :for num :in nums
+        :while (<= adjacent 2)
+        :when (number-adjacent-p i j num)
+          :do (incf adjacent)
+              (setf res (* res (val num)))
+        :finally (if (= adjacent 2)
+                     (return res)
+                     (return 0))))
+
+(defun answer-ex-3-1 ()
+  (let ((grid (read-file-as-array "../inputs/input3.txt")))
+    (loop :for num :in (get-all-numbers grid)
+          :when (number-valid-p grid num)
+            :sum (val num))))
+
+(defun answer-ex-3-2 ()
+  (let* ((grid (read-file-as-array "../inputs/input3.txt"))
+         (nums (get-all-numbers grid))
+         (acc 0))
+    (do-array (i j x grid)
+      (when (char= x #\*)
+        (incf acc (gear-p i j nums))))
+    acc))
