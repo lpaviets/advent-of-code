5976bfe53f8d4263f9844ab8f12ca6deea256b1d
Leo Paviet Salomon <leo.paviet.salomon@gmail.com>
2023-12-06 13:50:15 +0100

    Day 6

--- aoc2023/src/ex6.lisp
+++ aoc2023/src/ex6.lisp
@@ -1,6 +1,30 @@
 (in-package #:aoc2023/ex6)
 
-(defun answer-ex-6-1 ())
+;; Equation:
+;; - Total time T
+;; - Time held H
+;; Then distance is d = (T-H)H
+;; Number of points above some threshold R:
+;; (T-H)H > R  <=>  H^2 - TH + R < 0
+;; Which means (as a function of H):
+;; (T +- sqrt(T^2 - 4R))/2 are the root,
 
-(defun answer-ex-6-2 ())
+(defun count-solutions (time record)
+  (let* ((disc (sqrt (- (* time time) (* 4 record))))
+         (root1 (/ (- time (ceiling disc)) 2))
+         (root2 (/ (+ time (ceiling disc)) 2))
+         (gap (- (floor root2) (ceiling root1))))
+    ;; Need to count the integers between those, excluding the roots !
+    (if (integerp root1)
+        (1- gap)
+        (1+ gap))))
 
+(defun answer-ex-6-1 ()
+  (let ((params (read-file-as-lines "../inputs/input6.txt" :parse 'collect-integers-in-line)))
+    (reduce '* (mapcar 'count-solutions (first params) (second params)))))
+
+(defun answer-ex-6-2 ()
+  (let ((params (read-file-as-lines "../inputs/input6.txt"
+                                    :parse (lambda (line)
+                                             (parse-integer (remove-if-not 'digit-char-p line))))))
+    (count-solutions (first params) (second params))))
