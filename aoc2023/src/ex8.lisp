db563dcb930f91d8d15cd448a3c6fc7c460a2b4c
Leo Paviet Salomon <leo.paviet.salomon@gmail.com>
2023-12-08 10:05:14 +0100

    Poorly coded day 8 part 2

--- aoc2023/src/ex8.lisp
+++ aoc2023/src/ex8.lisp
@@ -4,6 +4,9 @@
 (defparameter *moves* nil)
 (defparameter *graph* nil)
 
+(defparameter *finished-p* nil)
+
+
 (defun read-node (line)
   (ppcre:register-groups-bind ((#'read-from-string node left right))
       ("^(\\w+) = \\((\\w+), (\\w+)\\)$" line)
@@ -16,27 +19,64 @@
     (setf (cdr (last moves)) moves)     ; circular movements
     (list moves (mapcar 'read-node graph))))
 
-(defun initialize-structures (moves graph)
-  (setf *current* 'aaa)
+(defun initialize-structures (moves graph init)
+  (setf *current* (coerce init 'vector))
+  (setf *finished-p* nil)
   (setf *moves* moves)
   (setf *graph* (make-hash-table :size (length graph) :test 'eq))
   (dolist (node graph)
     (setf (gethash (first node) *graph*) (second node))))
 
-(defun make-move (dir)
-  (let ((next (gethash *current* *graph*)))
-    (case dir
-      (#\L (setf *current* (car next)))
-      (#\R (setf *current* (cdr next))))
-    *current*))
+(defun node-start-p (node)
+  (char= (char (symbol-name node) 2) #\A))
+
+(defun node-end-p (node)
+  (char= (char (symbol-name node) 2) #\Z))
+
+(defun make-move (dir endp)
+  (setf *finished-p* t)
+  (loop :with dir-fun = (ecase dir
+                          (#\L 'car)
+                          (#\R 'cdr))
+        :for i :below (length *current*)
+        :for current = (aref *current* i)
+        :for next = (funcall dir-fun (gethash current *graph*))
+        :for next-end-p = (funcall endp next)
+        :unless next-end-p
+          :do (setf *finished-p* nil)
+        :collect (setf (aref *current* i) next)))
 
 (defun answer-ex-8-1 ()
-  (apply 'initialize-structures (parse-file "../inputs/input8.txt"))
+  (destructuring-bind (moves graph)
+      (parse-file "../inputs/input8.txt")
+    (initialize-structures moves graph (list 'aaa)))
   (loop :for dir :in *moves*
         :for i :from 0
-        :thereis (and (eq *current* 'zzz) i)
-        :do (make-move dir)))
+        :thereis (and *finished-p* i)
+        :do (make-move dir (lambda (node) (eq node 'zzz)))))
 
-;; Part 2
+(defun find-period (start)
+  (setf *finished-p* nil)
+  (let ((*current* (vector start)))
+    (loop :for dir :in *moves*
+          :for i :from 0
+          :thereis (and *finished-p* i)
+          :do (make-move dir 'node-end-p))))
 
-(defun answer-ex-8-2 ())
+(defun answer-ex-8-2 ()
+  (destructuring-bind (moves graph)
+      (parse-file "../inputs/input8.txt")
+    ;; (parse-file "test.txt")
+    (initialize-structures moves graph (remove-if-not 'node-start-p
+                                                      (mapcar 'car graph))))
+  ;; We remark that there is a (actual !) period:
+  ;; for every starting node n_i, there is a period
+  ;; p_i such that at step k*p_i the i-th "node" is on an
+  ;; ending node, for every k.
+  ;;
+  ;; We simply find this period for every node and compute their LCM
+  (let* ((n (length *current*))
+         (periods (make-array n :initial-element 0)))
+    (dotimes (i n)
+      (setf (aref periods i) (find-period (aref *current* i))))
+    (reduce 'lcm periods)))
