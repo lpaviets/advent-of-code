1e2fc4385f6d8d0ec29fa86cf26666045ae546f8
Leo Paviet Salomon <leo.paviet.salomon@gmail.com>
2023-12-09 09:41:13 +0100

    Day 9, quite easy

--- aoc2023/src/ex9.lisp
+++ aoc2023/src/ex9.lisp
@@ -1,6 +1,28 @@
 (in-package #:aoc2023/ex9)
 
-(defun answer-ex-9-1 ())
+(defun differences (list)
+  (mapcar '- (cdr list) list))
 
-(defun answer-ex-9-2 ())
+(defun extrapolate (list &key (backwards nil))
+  (let (all-differences)
+    (loop :for prev = list :then next
+          :for next = (differences prev)
+          :until (every 'zerop prev)
+          :do (push (if backwards prev (reverse prev))
+                    all-differences))
+    (reduce (if backwards
+                (lambda (x y) (- y x))
+                '+)
+            all-differences
+            :key 'car)))
 
+(defun extrapolate-backwards (list)
+  (extrapolate list :backwards t))
+
+(defun answer-ex-9-1 ()
+  (let ((lines (read-file-as-lines "../inputs/input9.txt" :parse 'collect-integers-in-line)))
+    (reduce '+ lines :key 'extrapolate)))
+
+(defun answer-ex-9-2 ()
+  (let ((lines (read-file-as-lines "../inputs/input9.txt" :parse 'collect-integers-in-line)))
+    (reduce '+ lines :key 'extrapolate-backwards)))
