6b758774976739ed7115f8a7f37727286fdefece
Leo Paviet Salomon <mail@lpaviets.org>
2024-12-01 08:53:43 +0100

    AoC 2024 Day 1

--- aoc2024/src/ex01/ex01.lisp
+++ aoc2024/src/ex01/ex01.lisp
@@ -3,6 +3,28 @@
 (defparameter *input* "input")
 (defparameter *test* "test")
 
-(defun answer-ex-1-1 ())
+(defun parse-file (file)
+  (let ((columns (read-file-as-sexprs file)))
+    (apply 'mapcar 'list columns))) ; this transposes
 
-(defun answer-ex-1-2 ())
+(defun count-occurrences (list &optional (test 'eql))
+  (let ((table (make-hash-table :test test)))
+    (dolist (x list table)
+      (incf (gethash x table 0)))))
+
+(defun answer-ex-1-1 (file)
+  (loop :with (left right) = (parse-file file)
+        :for x :in (sort left '<)
+        :for y :in (sort right '<)
+        :sum (abs (- x y))))
+
+(defun answer-ex-1-2 (file)
+  (loop :with (left right) = (parse-file file)
+        :with occurrences = (count-occurrences right)
+        :for x :in left
+        :sum (* x (gethash x occurrences 0))))
+
+;;;; Easier but slower solution, works as the file is small
+;; (defun answer-ex-1-2 (file)
+;;   (destructuring-bind (left right) (parse-file file)
+;;     (reduce '+ left :key (lambda (x) (* x (count x right))))))
