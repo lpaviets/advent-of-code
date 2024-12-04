7fc7364b0878f1ffe403d00eb36bade690a1afea
Leo Paviet Salomon <mail@lpaviets.org>
2024-12-04 07:41:04 +0100

    Start day 4

--- aoc2024/src/ex04/ex04.lisp
+++ aoc2024/src/ex04/ex04.lisp
@@ -3,7 +3,24 @@
 (defparameter *input* #P"input")
 (defparameter *test* #P"test")
 
-(defun answer-ex-4-1 (file))
+(defun match-xmas-at (puzzle i j di dj)
+  (loop :for c :across "XMAS"
+        :for x = i :then (+ x di)
+        :for y = j :then (+ y dj)
+        :while (array-in-bounds-p puzzle x y)
+        :always (char= c (aref puzzle x y))))
+
+(defun answer-ex-4-1 (file)
+  (let ((puzzle (read-file-as-array file))
+        (res 0))
+    (do-array (i j x puzzle)
+      (when (char= x #\X)
+        (loop :for di :in '(-1 0 1)
+              :do (loop :for dj :in '(-1 0 1)
+                        :unless (equalp '(0 0) `(,di ,dj))
+                          :when (match-xmas-at puzzle i j di dj)
+                            :do (incf res 1)))))
+    res))
+
 
 (defun answer-ex-4-2 (file))
-
