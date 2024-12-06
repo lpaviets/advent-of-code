6b6ab1fe3c228fa84189ec30e15bc00f2add0b91
Leo Paviet Salomon <mail@lpaviets.org>
2024-12-06 23:01:28 +0100

    Improve Day 6

--- aoc2024/src/ex06/ex06.lisp
+++ aoc2024/src/ex06/ex06.lisp
@@ -9,6 +9,7 @@
 (defun start-pos (grid)
   (do-array (i j x grid)
     (when (char= #\^ x)
+      (setf (aref grid i j) #\.)
       (return-from start-pos (list i j)))))
 
 (defun grid-turn-right (dir)
@@ -27,8 +28,7 @@
 
 (defun grid-free-cell-p (pos grid)
   (or (not (grid-valid-pos-p pos grid))
-      (char= (grid-at pos grid) #\.)
-      (char= (grid-at pos grid) #\^)))
+      (char= (grid-at pos grid) #\.)))
 
 (defun visited-cells-to-exit (start-pos grid)
   (let ((visited (make-array (array-dimensions grid) :initial-element nil))
@@ -46,20 +46,18 @@
     count))
 
 (defun detect-loop (start-pos grid)
-  (let ((visited (make-array (cons 4 (array-dimensions grid)) :initial-element nil))
-        (dir :up)
-        (pos start-pos))
-    (loop :for (x y) = pos
-          :for dirnum = (dir-to-num dir)
+  (let ((visited (make-array (cons 4 (array-dimensions grid)) :initial-element nil)))
+    (loop :for pos = start-pos :then (if next-free-p try-next pos)
+          :for dir = :up :then (if next-free-p dir (grid-turn-right dir))
+          :for (x y) = pos
+          :for try-next = (grid-pos-in-direction pos dir)
+          :for next-free-p = (grid-free-cell-p try-next grid)
+          :for dir-to-num = (dir-to-num dir)
           :while (grid-valid-pos-p pos grid)
-          :if (aref visited dirnum x y)
+          :if (aref visited dir-to-num x y)
             :do (return-from detect-loop t)
           :else
-            :do (setf (aref visited dirnum x y) t)
-                (let ((next (grid-pos-in-direction pos dir)))
-                  (if (grid-free-cell-p next grid)
-                      (setf pos next)
-                      (setf dir (grid-turn-right dir)))))
+            :do (setf (aref visited dir-to-num x y) t))
     nil))
 
 (defun loop-with-obstacle-p (start-pos obstacle-pos grid)
