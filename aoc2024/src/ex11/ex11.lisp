8abc5233378e05f809ca8053317a38a5fbb0e9d0
Leo Paviet Salomon <mail@lpaviets.org>
2024-12-11 09:14:08 +0100

    Day 11

--- aoc2024/src/ex11/ex11.lisp
+++ aoc2024/src/ex11/ex11.lisp
@@ -3,7 +3,57 @@
 (defparameter *input* #P"input")
 (defparameter *test* #P"test")
 
-(defun answer-ex-11-1 (file))
+(defun stone-list (file)
+  (read-file-one-line file :parse 'collect-integers-in-line))
 
-(defun answer-ex-11-2 (file))
+(defun digits-count (n)
+  (loop :for current = n :then q
+        :for (q r) = (multiple-value-list (truncate current 10))
+        :count 1
+        :while (plusp q)))
 
+(defun split-digits (n &optional (digits (digits-count n)))
+  (truncate n (expt 10 (/ digits 2))))
+
+(defun evolve-step (stones)
+  (let (new-stones digits)
+    (dolist (stone stones new-stones)
+      (cond
+        ((zerop stone) (push 1 new-stones))
+        ((evenp (setf digits (digits-count stone)))
+         (multiple-value-bind (left right)
+             (split-digits stone digits)
+           (push left new-stones)
+           (push right new-stones)))
+        (t (push (* 2024 stone) new-stones))))))
+
+(defun %count-stones-after (stone steps cache)
+  (if (zerop steps)
+      1
+      (let ((data (cons stone steps)))
+        (or (gethash data cache)
+            (setf (gethash data cache)
+                  (let ((digits (digits-count stone)))
+                    (cond
+                      ((zerop stone) (%count-stones-after 1 (1- steps) cache))
+                      ((evenp digits)
+                       (multiple-value-bind (left right)
+                           (split-digits stone digits)
+                         (+ (%count-stones-after left (1- steps) cache)
+                            (%count-stones-after right (1- steps) cache))))
+                      (t (%count-stones-after (* 2024 stone) (1- steps) cache)))))))))
+
+(defun count-stones-after (stones steps)
+  (let ((cache (make-hash-table :test 'equal)))
+    (loop :for stone :in stones
+          :sum (%count-stones-after stone steps cache))))
+
+(defun answer-ex-11-1 (file)
+  (let ((stones (stone-list file)))
+    (dotimes (_ 25)
+      (setf stones (evolve-step stones)))
+    (length stones)))
+
+(defun answer-ex-11-2 (file)
+  (let ((stones (stone-list file)))
+    (count-stones-after stones 75)))
