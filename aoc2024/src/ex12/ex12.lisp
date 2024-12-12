b27b357c9ce1e3ff6788b848be3eb51204643c01
Leo Paviet Salomon <mail@lpaviets.org>
2024-12-12 14:32:44 +0100

    Day 12 both parts

--- aoc2024/src/ex12/ex12.lisp
+++ aoc2024/src/ex12/ex12.lisp
@@ -3,7 +3,94 @@
 (defparameter *input* #P"input")
 (defparameter *test* #P"test")
 
-(defun answer-ex-12-1 (file))
+(defun parse-garden (file)
+  (read-file-as-array file))
 
-(defun answer-ex-12-2 (file))
+;; Way simpler solution than what I did: loop by lines, then by columns.
+;; Keep track of the "current region"
+;;
 
+(defun plot-region-neighbours (plot garden)
+  (loop :with plant = (grid-at plot garden)
+        :for nghb :in (grid-neighbours plot garden)
+        :for nghb-plant = (grid-at nghb garden)
+        :when (eql plant nghb-plant)
+          :collect nghb))
+
+(defun graph-edges-from-garden (garden)
+  (let ((cache-array (make-array (array-dimensions garden) :initial-element nil)))
+    (do-array (i j x garden)
+      (loop :for nghb :in (plot-region-neighbours (list i j) garden)
+            :do (push (cons nghb 1) (aref cache-array i j))))
+    (lambda (pos)
+      (apply 'aref cache-array pos))))
+
+(defun count-plot-borders (plot garden)
+  (let* ((neighbours (grid-neighbours plot garden))
+         (walls (- 4 (length neighbours))) ; outside the grid: not returned by
+                                        ; `grid-neighbours'
+         (neighbours-outside (loop :with plant = (grid-at plot garden)
+                                   :for nghb :in (grid-neighbours plot garden)
+                                   :count (char/= plant (grid-at nghb garden)))))
+    (+ walls neighbours-outside)))
+
+(defun garden-regions (garden)
+  (let ((edges (graph-edges-from-garden garden))
+        (all-plots nil))
+    (do-array (i j x garden)
+      (push (list i j) all-plots))
+    (connected-components all-plots edges :test 'equalp)))
+
+(defun perimeter (region garden)
+  (let ((total 0))
+    (do-hashkeys (plot region)
+      (incf total (count-plot-borders plot garden)))
+    total))
+
+(defun area (region)
+  (hash-table-count region))
+
+;;; Part 2
+
+;;; Idea of the computation:
+;;; - A side <=> a corner
+;;; - A 'out'-corner <=> two 'consecutive' sides adjacent to the exterior
+;;; - A 'in'-corner  <=> two consecutive sides in the same region, but diagonal is not
+
+(defun neighbour-in-region-p (plot region dir)
+  (gethash (grid-pos-in-direction plot dir) region))
+
+(defun count-out-corners (plot region)
+  (loop :for (dir1 dir2) :on '(:up :right :down :left :up)
+        :while dir2
+        :count (and (not (neighbour-in-region-p plot region dir1))
+                    (not (neighbour-in-region-p plot region dir2)))))
+
+(defun count-in-corners (plot region)
+  (loop :for (dir1 dir2) :on '(:up :right :down :left :up)
+        :while dir2
+        :count (and (neighbour-in-region-p plot region dir1)
+                    (neighbour-in-region-p plot region dir2)
+                    (not (neighbour-in-region-p (grid-pos-in-direction plot dir1) region dir2)))))
+
+(defun count-corners (plot region)
+  (+ (count-in-corners plot region)
+     (count-out-corners plot region)))
+
+(defun sides (region)
+  (let ((sides 0))
+    (do-hashkeys (plot region)
+      (incf sides (count-corners plot region)))
+    sides))
+
+(defun answer-ex-12-1 (file)
+  (let ((garden (parse-garden file)))
+    (loop :for region :in (garden-regions garden)
+          :sum (* (area region)
+                  (perimeter region garden)))))
+
+(defun answer-ex-12-2 (file)
+  (let ((garden (parse-garden file)))
+    (loop :for region :in (garden-regions garden)
+          :sum (* (area region)
+                  (sides region)))))
