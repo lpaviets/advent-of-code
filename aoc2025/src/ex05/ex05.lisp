b7f20e208aeddd83cff813f45e83f8b7a9160252
Leo Paviet Salomon <mail@lpaviets.org>
2025-12-05 11:01:00 +0100

    Day 5 : already coded the interval stuff before

--- aoc2025/src/ex05/ex05.lisp
+++ aoc2025/src/ex05/ex05.lisp
@@ -3,7 +3,31 @@
 (defparameter *input* #P"input")
 (defparameter *test* #P"test")
 
-(defun answer-ex-5-1 (file))
+;; Stupid P1
+(defun read-intervals (file)
+  (destructuring-bind (ranges ids)
+      (read-file-as-lines-blocks file)
+    (list (mapcar (lambda (range)
+                    (destructuring-bind (low high)
+                        (mapcar 'parse-integer (cl-ppcre:split "-" range))
+                      (make-interval low high)))
+                  ranges)
+          (mapcar 'parse-integer ids))))
 
-(defun answer-ex-5-2 (file))
+(defun freshp (id ranges)
+  (loop :for interval :in ranges
+          :thereis (interval-contains-p interval id)))
 
+(defun range-to-interval (range)
+  (make-interval (first range) (second range)))
+
+(defun answer-ex-5-1 (file)
+  (destructuring-bind (ranges ids)
+      (read-intervals file)
+    (loop :for id :in ids
+          :count (freshp id ranges))))
+
+(defun answer-ex-5-2 (file)
+  (let ((intervals (car (read-intervals file))))
+    (reduce '+ (apply 'interval-union intervals)
+            :key 'interval-cardinal)))
